e7a19054f0cf4b50ed62fa4569451fbfb2f64736
Thiago Alvoravel <t.alvoravel@gmail.com>
2020-10-16 18:41:15 -0300

    Uso do ErrorCollector na etapa de validacao

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTeste.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTeste.java
@@ -1,9 +1,16 @@
 package br.ce.wcaquino.servicos;
 
 import java.util.Date;
+import java.util.Objects;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -13,15 +20,22 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTeste {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void teste() {
+		//cenário
 		Usuario usuario = new Usuario("João");
 		Filme filme = new Filme("Tubarão", 3, 2.5);
 		LocacaoService locacaoService = new LocacaoService();
+		
+		//acao
 		Locacao locacaoTubarao = locacaoService.alugarFilme(usuario, filme);
 		
-		Assert.assertTrue(locacaoTubarao.getValor() == 2.5);
-		Assert.assertTrue(DataUtils.isMesmaData(locacaoTubarao.getDataLocacao(), new Date()));
+		//verificação
+		error.checkThat(filme.getPrecoLocacao(), is(equalTo(locacaoTubarao.getValor())));
+		error.checkThat(DataUtils.isMesmaData(locacaoTubarao.getDataLocacao(), new Date()), is(true));
 		
 	}
 
